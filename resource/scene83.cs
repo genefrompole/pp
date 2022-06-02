19c06292736421167f2a524e39855dc1472b790a
genefrompole <106128887+genefrompole@users.noreply.github.com>
2022-06-02 22:45:05 +0900

    ch_3

---
+++ resource/scene83.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene83 : MonoBehaviour
+{
+    public void GoGameScene()
+    {
+        SceneManager.LoadScene("game-8");
+    }
+
+}
